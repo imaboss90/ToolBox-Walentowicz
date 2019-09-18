8b112d424f2d63bc8de9fdcaedce48f7d82aea90
imaboss90 <54406879+imaboss90@users.noreply.github.com>
2019-09-18 13:48:32 -0500

    intent

--- app/src/main/java/com/example/toolbox/MainActivity.java
+++ app/src/main/java/com/example/toolbox/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.toolbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void order(View v){
+        EditText myName = (EditText) findViewById(R.id.nameEdit);
+        EditText myEmail = (EditText) findViewById(R.id.emailEdit);
+
+        String myNameStr = myName.getText().toString();
+        String myEmailStr = myEmail.getText().toString();
+
+        Intent intent = new Intent(this, InfoActivity.class);
+
+        //intent.putExtra(InfoActivity.MY_NAME, myNameStr);
+        //intent.putExtra(InfoActivity.MY_NOUN, myNounStr);
+
+        startActivity(intent);
+
+
+    }
+
 }
